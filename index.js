6c87f30da271058918f41af6eeaec8eacb0add66
gillesdemey <gilles.de.mey@gmail.com>
2020-11-09 22:37:44 +0100

    feat: support LightMyRequest objects

--- index.js
+++ index.js
@@ -3,11 +3,27 @@
 const sortKeys = require('sort-keys')
 const toSchema = require('generate-schema').json
 
+const isSupertest = res => res.status && res.body
+const isLightMyRequest = res => res.statusCode && res.body
+
 const JSONRestAPISnapshotSerializer = {
-  test: (res) => res.status && res.body,
+  test: (res) => [
+    isSupertest,
+    isLightMyRequest
+  ].some(testFn => testFn(res)),
   print: ({ body }) => {
-    const schema = sortKeys(toSchema(body), { deep: true })
+    const contents = tryParse(body)
+    const schema = sortKeys(toSchema(contents), { deep: true })
     return JSON.stringify(schema, null, 2)
+  }
+}
+
+// work for string values and Buffers
+function tryParse (value) {
+  try {
+    return JSON.parse(value)
+  } catch (error) {
+    return value
   }
 }
 
--- test.js
+++ test.js
@@ -13,3 +13,13 @@ test('should serialize HTTP response', () => {
   const object = { status: 200, body: { hello: 'world' } }
   expect(object).toMatchSnapshot()
 })
+
+test('should serialize HTTP Buffer response', () => {
+  const object = { statusCode: 200, body: Buffer.from('{ "Hello": "World" }') }
+  expect(object).toMatchSnapshot()
+})
+
+test('should serialize LightMyRequest HTTP response', () => {
+  const object = { statusCode: 200, body: '{ "Hello": "World" }' }
+  expect(object).toMatchSnapshot()
+})
